2827d3bfb7df6b83b99b69474cc3c2bef8825940
HuaJiKing402 <huajiking402@outlook.com>
2025-04-27 01:31:40 +0800

    first commit
    Signed-off-by: HuaJiKing402 <huajiking402@outlook.com>

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script lang="ts" setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <main>
-    <TheWelcome/>
+    <input autocomplete="username webauthn" type="text"/>
+    <input autocomplete="new-password" type="password">
   </main>
 </template>
+
+<script lang="ts">
+</script>
